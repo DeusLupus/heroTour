8d6bd3ff38053fdf2932fc503e6785807f24363c
Deo Bissessar <bissessardeo@gmail.com>
2018-11-09 01:34:23 -0500

    displayed list, select to view, ngfor, ngif, toggle css style class with class binding

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Deus Lupus' },
+	{ id: 12, name: 'Jinx' },
+	{ id: 13, name: 'Natura' },
+	{ id: 14, name: 'Vixen' },
+	{ id: 15, name: 'Deathstalker' },
+	{ id: 16, name: 'Ghostdevil' },
+	{ id: 17, name: 'Nightstalker' },
+	{ id: 18, name: 'Dominus' },
+	{ id: 19, name: 'Styx' },
+	{ id: 20, name: 'Living Death' }
+];
